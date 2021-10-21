359cb4971214a2886aad6d2cb6547aba07771d49
Tyler-Landau <70411589+Tyler-Landau@users.noreply.github.com>
2021-10-20 22:07:45 -0500

    Changes to be committed: 	new file:   AuctionContractQuestionableAdditions.txt 	new file:   src/contracts/PawAuction.sol 	new file:   src/contracts/PawAuctionFactory.sol

---
+++ src/contracts/PawAuction.sol
@@ -0,0 +1,4 @@
+// This sample PawAuction contract will be an example and template only contract.
+// Destiny Graham and Tyler Landau will be collaborating on this contract with additional tutelage
+// from James Tackett and Faith Nice
+
