60d84bd61b6f2c868140bc90052fa383dc78edcf
James Tackett <43919873+Shreaderdog@users.noreply.github.com>
2021-10-24 13:45:14 -0500

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 const CryptoPaws = artifacts.require("./CryptoPaws.sol");
 var PawAuctionFactory = artifacts.require("./PawAuctionFactory.sol");
 var PawAuction = artifacts.require("./PawAuction.sol");
-=======
-const CryptoPaws = artifacts.require("./CryptoPaws");
-var PawAuctionFactory = artifacts.require("./PawAuctionFactory.sol");
-var PawAuction = artifacts.require("./PawAuction.sol")
->>>>>>> 70733ce65cdc2daab70eefe5f517706fecdf48f3
 
 module.exports = async function(deployer) {
   await deployer.deploy(CryptoPaws);
   await deployer.deploy(PawAuctionFactory);
-<<<<<<< HEAD
   await deployer.deploy(PawAuction);
 
-=======
-  await deployer.deploY(PawAuction);
->>>>>>> 70733ce65cdc2daab70eefe5f517706fecdf48f3
 };
 
 
