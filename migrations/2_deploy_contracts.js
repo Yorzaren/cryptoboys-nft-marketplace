696a4d199c5837058fba66d5f79d648865dde77f
destinyg1508 <dtg1107@gmail.com>
2021-11-30 16:33:05 -0600

    Update 2_deploy_contracts.js
    updated to deploy auction contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const CryptoPaws = artifacts.require("CryptoPaws");
 const lottery = artifacts.require("Lottery.sol");
 const lotteryfactory = artifacts.require("LotteryFactory.sol");
+const auction = artifacts.require("PawAuction.sol");
+const auctionfactory = artifacts.require("PawAuctionFactory.sol");
 
 module.exports = async function(deployer) {
   let pawContract = await deployer.deploy(CryptoPaws);
   let lotterycontract = await deployer.deploy(lottery);
   let factore = await deployer.deploy(lotteryfactory, CryptoPaws.address, lottery.address);
+  let auctioncontract = await deployer.deploy(auction);
+  let factory = await deployer.deploy(auctionfactory, CryptoPaws.address, auction.address);
 };
